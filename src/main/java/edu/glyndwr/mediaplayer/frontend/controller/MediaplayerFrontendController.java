a18b83b7d6987925d95df82b0bcfd13de7450701
discosnik <snik@nightgoblinworldorder.com>
2019-02-13 23:36:57 +0100

    added ClassDiagram

--- src/main/java/edu/glyndwr/mediaplayer/frontend/controller/MediaplayerFrontendController.java
+++ src/main/java/edu/glyndwr/mediaplayer/frontend/controller/MediaplayerFrontendController.java
@@ -4,9 +4,16 @@ import edu.glyndwr.mediaplayer.backend.mediaservice.integration.controller.YouTu
 import edu.glyndwr.mediaplayer.backend.mediaservice.integration.models.YouTubeSearchQuery;
 import edu.glyndwr.mediaplayer.backend.mediaservice.integration.models.YouTubeVideo;
 import edu.glyndwr.mediaplayer.frontend.factories.MediaPlayerUIFactory;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
@@ -14,6 +21,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 /**
@@ -36,15 +44,25 @@ public class MediaplayerFrontendController {
     private Stage primaryStage;
     private GridPane resultPane;
     private ArrayList<YouTubeVideo> videos = new ArrayList<>();
-    
-    
+
     public void initializeStage(Stage primaryStage) {
         initializeFields(primaryStage);
-        uiFactory.buildUI(this).show();
-        
+        primaryStage = uiFactory.buildUI(this);
+        InputStream icon = getClass().getResourceAsStream("icon.png");
+        if (null != icon) {
+            Image imageIcon = new Image(icon);
+            if (imageIcon != null) {
+                primaryStage.getIcons().add(imageIcon);
+            }else{
+                log.info("imageIcon is null! no Icon!");
+            }
+        } else {
+            log.info("Icon inputStream is null! no Icon!");
+        }
+
+        primaryStage.show();
+
     }
-
-
 
     public void loadVideos(String query) {
         youTubeIntegrationController.setYoutubeSearchCriteria(new YouTubeSearchQuery());
@@ -60,13 +78,15 @@ public class MediaplayerFrontendController {
         uiFactory.rebuildPlayerPane(video);
         this.player.getEngine().loadContent(video.getEmbedCode());
     }
-    private void initializeFields(Stage primaryStage1) {
+
+    private void initializeFields(Stage primaryStage) {
+
         this.resultPane = new GridPane();
         this.resultPane.setId("resultpane");
         this.player = new WebView();
         this.player.setPrefSize(660, 380);
-        this.searchField =  new TextField();
+        this.searchField = new TextField();
         this.searchButton = new Button();
-        this.primaryStage = primaryStage1;
+        this.primaryStage = primaryStage;
     }
 }
